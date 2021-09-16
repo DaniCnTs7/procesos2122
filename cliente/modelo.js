2356abc14ac09ea37a94dfcbc6b61d39cfc176c8
DANIEL CANTOS GARCIA <Daniel.Cantos@alu.uclm.es>
2021-09-16 19:25:48 +0200

    nuevas clases partida y carta

--- cliente/modelo.js
+++ cliente/modelo.js
@@ -3,14 +3,24 @@ function Juego() {
     this.jugadores = {}
 
     this.agregarJugadores = (nick) => {
-        console.log(this.jugadores[nick])
         if (!this.jugadores[nick]) {
             var jugador = new Jugador(nick)
             this.jugadores[nick] = jugador
+        } else {
+            alert("Ya existe el jugador")
         }
     }
 }
 
 function Jugador(nick) {
-    
+    this.nick = nick
+}
+
+function Partida(nombre) {
+    this.nombre = nombre;
+}
+
+function Carta(color, tipo) {
+    this.color = color
+    this.tipo = tipo
 }
